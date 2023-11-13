96407038cc1be4e96acafdd407f8050455f826e4
twocucao <twocucao@gmail.com>
2023-11-13 18:40:44 +0800

    add automate workflow

--- projects/002_EnjiPainting/gen_upscale.bat
+++ projects/002_EnjiPainting/gen_upscale.bat
@@ -3,7 +3,7 @@ REM Get dropped item folder name
 for %%I in ("%~dp1") do set "folder=%%~nI"
 
 REM Get the current directory
-cd /d %~dp0\..
+cd /d %~dp0\..\..
 
 REM Python command
 SET "cmd=.\venv\Scripts\python -m animatediff tile-upscale -W 1024 %*  -o .\projects\%folder%\upscaled"
