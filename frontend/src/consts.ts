1f2f53bd86fe71a200f81fd81c8dc346eeecf29a
twocucao <twocucao@gmail.com>
2023-12-02 22:31:17 +0800

    fix progressbar

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,5 +1,4 @@
 // export const urlPrefix = "http://localhost:8000"
 // export const urlPrefix = ""
-export const urlPrefix = "http://192.168.3.27:7860"
 export const API_URL = "http://192.168.3.27:7860/api"
 export const MEDIA_URL = "http://192.168.3.27:7860/media"
